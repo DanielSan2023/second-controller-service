a903b6cf90491ceff426b743d7752e248dbf55e6
Daniel_San <electronic.san@gmail.com>
2023-12-02 12:51:55 +0100

    added Dependency Injection

--- src/main/java/com/engeto/controller/StudentController.java
+++ src/main/java/com/engeto/controller/StudentController.java
@@ -1,4 +1,65 @@
 package com.engeto.controller;
 
+import com.engeto.model.Student;
+import com.engeto.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class StudentController {
+
+    @Autowired // Dependency injection - injekt class Student Service try konstructor
+    private StudentService studentService; // jeden objekt pre  celu triedu
+
+
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        // 1.return studentService.createAndSendStudents();
+//2.       List<Student> students = studentService.returnStudents();
+//2.        return students;
+      return studentService.returnStudents();
+
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable("id") Long id) {
+        Student student = new Student();
+        student.setName("Daniel");
+        student.setSurname("San");
+        student.setAge(39);
+        student.setId(id);
+        return student;
+    }
+
+    @PostMapping("/students")
+    public Student createStudent(@RequestBody Student student) {
+        //student.getName;
+        studentService.createNewStudent(student);
+        return student;
+    }
+
+    @DeleteMapping("/students/{id}")
+    public void deleteStudent(@PathVariable("id") Long id) {
+       studentService.deleteStudent(id);
+    }
+
+    @PutMapping("/students/{id}")
+    public void updateStudent(@PathVariable("id") Long id, @RequestBody Student student) {
+       studentService.updateStudent(id, student);
+    }
+
+    @GetMapping("/students/create") //@PostMapping instead @GetMapping
+    public void createDefaultStudents() {
+        studentService.createDefaultStudents();
+    }
 }
+
+
+
+
+
+
+
--- src/main/java/com/engeto/service/StudentService.java
+++ src/main/java/com/engeto/service/StudentService.java
@@ -1,7 +1,55 @@
 package com.engeto.service;
 
+import com.engeto.model.Student;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StudentService {
+
+
+    private List<Student> students = new ArrayList<>();
+
+    public void createDefaultStudents() {
+        Student student1 = new Student();
+        student1.setName("Daniel");
+        student1.setSurname("San");
+        student1.setAge(39);
+        student1.setId(1L);
+
+        Student student2 = new Student();
+        student2.setName("Bruce");
+        student2.setSurname("Lee");
+        student2.setAge(59);
+        student2.setId(2L);
+
+        students.add(student1);
+        students.add(student2);
+
+    }
+
+    public List<Student> returnStudents() {
+        return students;
+    }
+
+
+    public void createNewStudent(Student student) {
+        students.add(student);
+    }
+
+    public void deleteStudent(Long id) {
+        students.removeIf(student -> student.getId().equals(id));
+    }
+
+    public void updateStudent(Long id, Student student) {
+        students.forEach(student1 -> {
+            if (student1.getId().equals(id)) {
+                student1.setName(student.getName());
+                student1.setSurname(student.getSurname());
+                student1.setAge(student.getAge());
+            }
+        });
+    }
 }
